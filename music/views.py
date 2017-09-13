d816ab6971ef6662f3c29339332a3300c6b02d18
AHAMMED SHANEEB N K <shaneeb2015@gmail.com>
2017-09-14 01:07:35 +0530

    Templates - 4 (Http4O4 error message)

--- music/views.py
+++ music/views.py
@@ -2,10 +2,6 @@
 # user request something probably a web page or some code and returns something
 # or something such as logout, download etc.
 # Create your views here.
-# deleting 'from django.shortcuts import render' ; don't know why
-# there are many methods of giving response, simple one is http response
-# writing import statement as 'from django.http import HttpResponse'
-from django.http import HttpResponse
 # we want to use information from Album database so need to import it
 from .models import Album
 # for easy development, it is good to seperate frontend and backend
@@ -13,6 +9,11 @@ from .models import Album
 # combining template loading and running by importing render from shortcuts
 # and also avoiding HttpResponse and using render() function
 from django.shortcuts import render
+# for displaying 4O4 error we have to import it
+from django.http import Http404
+
+
+# 4O4 usually means requested thing is not existing in the resources
 
 # define the function named as index
 # example
@@ -21,16 +22,19 @@ from django.shortcuts import render
 def index(request):
     # we want to display the list of all albums. We access it by python command and assign it into a variable
     all_albums = Album.objects.all()
-    # creating something as dictionary. more often it is named as context
-    # context means information that our template needs
-    context = {'all_albums': all_albums}
     # view returns response to the request
-    return render(request, 'music/index.html', context)
+    return render(request, 'music/index.html', {'all_albums': all_albums})
     # actually HttpResponse is binded with render() function inbuilt
 
 # for /music/album_id/ eg:- /music/45/ so we need to pass album_id as parameter
 # so we need to connect with database and pull the data
+# check the requested album_id existing and displaying respective responses
 def detail(request, album_id):
-    return HttpResponse("<h2>Details of Album ID: " + str(album_id) + "</h2>")
+    try:
+        album = Album.objects.get(id=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album doesn't exist")
+    return render(request, 'music/detail.html', {'album': album})
+
 
 # album_id is integer. We convert it into a string by str() function
