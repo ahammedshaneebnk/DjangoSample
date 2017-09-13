accc0af7933a8704145f209eb7a3d4344a7e9a7c
AHAMMED SHANEEB N K <shaneeb2015@gmail.com>
2017-09-13 18:15:53 +0530

    Another view - 1

--- music/urls.py
+++ music/urls.py
@@ -1,5 +1,5 @@
 # this file deals the url mapping of this app
-# this then willbe imported to main page url.py page
+# this then will be imported to main page url.py page
 # copy the first line import statement of main urls.py page to here
 from django.conf.urls import url
 # then import views of current directory
@@ -7,8 +7,16 @@ from django.conf.urls import url
 from . import views
 
 # copy the syntax structure from main urls.py page and edit it.
+# r'' means regular expression
 # start with ^ and end with $
 urlpatterns = [
+    # /music
     url(r'^$', views.index, name='index'),
     # then in views.py file, create a function named as index and tell the response there
+
+    # we need something like this /music/albumId/ for album details eg:- /music/21/
+    # for it we need to group the ids together and call it as variable
+    # here, (?P<album_id>) is used. ie, in actual, album_id replaces the original id of Album
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+
 ]
--- music/views.py
+++ music/views.py
@@ -1,5 +1,5 @@
 # views are python functions
-# user request something probably a webpage and something returns
+# user request something probably a web page or some code and returns something
 # or something such as logout, download etc.
 # Create your views here.
 # deleting 'from django.shortcuts import render' ; don't know why
@@ -11,4 +11,11 @@ from django.http import HttpResponse
 # define the function named as index
 # example
 def index(request):
-    return HttpResponse("<h1>This is music app home page</h1>")
+    return HttpResponse("<h1>This is music app home page</h1>"
+                        "<h3>Here you can see a list of all albums</h3>")
+
+
+# for /music/album_id/ eg:- /music/45/ so we need to pass album_id as parameter
+# so we need to connect with database and pull the data
+def detail(request, album_id):
+    return HttpResponse("<h2>Details of Album ID: " + str(album_id) + "</h2>")
